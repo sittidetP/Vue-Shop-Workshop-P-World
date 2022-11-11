5b712d74d340e142c2968742a0c83dd8da641f1e
sittidetP <sittidet@odds.team>
2022-11-12 00:56:46 +0700

    add version in HomeView v1.0

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <div class="navbar bg-blue-500 flex justify-center">
     <div class="text-2xl text-white">My Shop</div>
   </div>
+  <p>v1.0</p>
   <div class="p-4 lg:p-8">
     <RouterView></RouterView>
   </div>
